4cd03065cb7585bc0b6d7d2ddc0aaab4dcd536ed
DragonflyStats <kobriendublin@gmail.com>
2014-08-28 21:59:21 +0100

    Create DF1.py

---
+++ Code/DF1.py
@@ -0,0 +1,18 @@
+data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+        'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions', 'Lions', 'Lions'],
+        'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+        'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+football = pd.DataFrame(data, columns=['year', 'team', 'wins', 'losses'])
+print football
+
+
+
+from_csv = pd.read_csv('mariano-rivera.csv')
+from_csv.head()
+
+cols = ['num', 'game', 'date', 'team', 'home_away', 'opponent',
+        'result', 'quarter', 'distance', 'receiver', 'score_before',
+        'score_after']
+no_headers = pd.read_csv('peyton-passing-TDs-2012.csv', sep=',', header=None,
+                         names=cols)
+no_headers.head()
