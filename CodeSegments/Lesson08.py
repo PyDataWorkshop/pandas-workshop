568ed9c6ea2a09d3c15fa92f533fc9f9b0cf850d
DragonflyStats <kobriendublin@gmail.com>
2016-08-23 23:00:38 +0100

    Update and rename Lesson8.py to Lesson08.py

--- CodeSegments/Lesson08.py
+++ CodeSegments/Lesson08.py
@@ -1,14 +1,13 @@
 
+## Lesson 8
+## How to pull data from a microsoft sql database
 
-Lesson 8
-How to pull data from a microsoft sql database
-
-In [1]:
+##############################################
 # Import libraries
 import pandas as pd
 import sys
 from sqlalchemy import create_engine, MetaData, Table, select
-In [2]:
+##############################################
 print('Python version ' + sys.version)
 print('Pandas version ' + pd.__version__)
 Python version 3.4.3 |Anaconda 2.4.0 (64-bit)| (default, Dec  1 2015, 11:39:45) [MSC v.1600 64 bit (AMD64)]
@@ -53,34 +52,13 @@ Select the contents in the dataframe.
 
 In [15]:
 df.head()
-Out[15]:
-DateSK	Date	Day	DaySuffix	DayOfWeek	DOWInMonth	DayOfYear	WeekOfYear	WeekOfMonth	Month	MonthName	Quarter	QuarterName	Year	StandardDate	HolidayText
-0	20000101	2000-01-01	1	1st	Saturday	1	1	1	1	1	January	1	First	2000	01/01/2000	New Year's Day
-1	20000102	2000-01-02	2	2nd	Sunday	1	2	2	2	1	January	1	First	2000	01/02/2000	None
-2	20000103	2000-01-03	3	3rd	Monday	1	3	2	2	1	January	1	First	2000	01/03/2000	None
-3	20000104	2000-01-04	4	4th	Tuesday	1	4	2	2	1	January	1	First	2000	01/04/2000	None
-4	20000105	2000-01-05	5	5th	Wednesday	1	5	2	2	1	January	1	First	2000	01/05/2000	None
+##############################################
 In [16]:
 df.dtypes
-Out[16]:
-DateSK                   int64
-Date            datetime64[ns]
-Day                      int64
-DaySuffix               object
-DayOfWeek               object
-DOWInMonth               int64
-DayOfYear                int64
-WeekOfYear               int64
-WeekOfMonth              int64
-Month                    int64
-MonthName               object
-Quarter                  int64
-QuarterName             object
-Year                    object
-StandardDate            object
-HolidayText             object
-dtype: object
-Convert to specific data types. The code below will have to be modified to match your table.
+##############################################
+
+## Convert to specific data types. 
+## The code below will have to be modified to match your table.
 
 In [17]:
 # Convert data types 
@@ -107,6 +85,8 @@ Year                     int32
 StandardDate    datetime64[ns]
 HolidayText             object
 dtype: object
+
+##############################################
 Version 2
 In [18]:
 import pandas.io.sql
@@ -136,8 +116,8 @@ DateSK	Date	Day	DaySuffix	DayOfWeek	DOWInMonth	DayOfYear	WeekOfYear	WeekOfMonth	
 3	20000104	2000-01-04	4	4th	Tuesday	1	4	2	2	1	January	1	First	2000	01/04/2000	None
 4	20000105	2000-01-05	5	5th	Wednesday	1	5	2	2	1	January	1	First	2000	01/05/2000	None
 
-
-Version 3
+##############################################
+## Version 3
 In [20]:
 from sqlalchemy import create_engine
 In [21]:
