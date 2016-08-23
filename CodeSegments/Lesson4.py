e24a59127fd1984eab19f72c90af9e7049d7f33f
DragonflyStats <kobriendublin@gmail.com>
2016-08-23 22:37:07 +0100

    Create Lesson4.py

---
+++ CodeSegments/Lesson4.py
@@ -0,0 +1,104 @@
+## Lesson 4
+In this lesson were going to go back to the basics. We will be working with a small data set so that you can easily understand what I am trying to explain. We will be adding columns, deleting columns, and slicing the data many different ways. Enjoy!
+
+# Import libraries
+import pandas as pd
+import sys
+##  -- Part2:
+print('Python version ' + sys.version)
+print('Pandas version: ' + pd.__version__)
+Python version 3.4.3 |Anaconda 2.4.0 (64-bit)| (default, Dec  1 2015, 11:39:45) [MSC v.1600 64 bit (AMD64)]
+Pandas version: 0.17.1
+
+
+##  -- Part3:
+# Our small data set
+d = [0,1,2,3,4,5,6,7,8,9]
+
+# Create dataframe
+myDF = pd.DataFrame(d)
+myDF
+
+
+##  -- Part4:
+# Lets change the name of the column
+myDF.columns = ['Rev']
+myDF
+
+
+##  -- Part5:
+# Lets add a column
+myDF['NewCol'] = 5
+myDF
+
+##  -- Part6:
+# Lets modify our new column
+myDF['NewCol'] = myDF['NewCol'] + 1
+myDF
+
+##  -- Part7:
+# We can delete columns
+del myDF['NewCol']
+myDF
+
+
+##  -- Part8:
+# Lets add a couple of columns
+myDF['test'] = 3
+myDF['col'] = myDF['Rev']
+myDF
+
+
+##  -- Part9:
+# If we wanted, we could change the name of the index
+i = ['a','b','c','d','e','f','g','h','i','j']
+myDF.index = i
+myDF
+
+We can now start to select pieces of the dataframe using loc.
+
+##  -- Part10:
+myDF.loc['a']
+
+##  -- Part11:
+# myDF.loc[inclusive:inclusive]
+myDF.loc['a':'d']
+
+
+##  -- Part12:
+# myDF.iloc[inclusive:exclusive]
+# Note: .iloc is strictly integer position based. 
+# It is available from [version 0.11.0] (http://pandas.pydata.org/pandas-docs/stable/whatsnew.html#v0-11-0-april-22-2013) 
+myDF.iloc[0:3]
+
+
+We can also select using the column name.
+
+##  -- Part13:
+myDF['Rev']
+
+
+##  -- Part14:
+myDF[['Rev', 'test']]
+
+##  -- Part15:
+# myDF.ix[rows,columns]
+myDF.ix[0:3,'Rev']
+
+Name: Rev, dtype: int64
+##  -- Part16:
+myDF.ix[5:,'col']
+
+myDF.ix[:3,['col', 'test']]
+
+################################
+
+## There is also some handy function to select the top and bottom records of a dataframe.
+
+##  -- Part18:
+# Select top N number of records (default = 5)
+myDF.head()
+
+##  -- Part19:
+# Select bottom N number of records (default = 5)
+myDF.tail()
