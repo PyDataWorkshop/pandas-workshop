1fd9412ccd5de45adf9d85d75024f7d1cb5fd156
DragonflyStats <kobriendublin@gmail.com>
2016-08-24 23:48:52 +0100

    Update Lesson08-SpecialArrays.py

--- numpy/Lesson08-SpecialArrays.py
+++ numpy/Lesson08-SpecialArrays.py
@@ -112,7 +112,22 @@ np.identity(5.7)
 N=5
 
 In = np.eye(N)
-​
-In
 
-​
+In
+#############################################################################
+
+M, N = 5, 5
+x = ones((M,N)) # M by N array of 1s
+x =  ones((M,M,N)) # 3D array
+x =  ones((M,N), dtype='int32') # 32-bit integers
+
+x = zeros((M,N)) # M by N array of 0s
+x = zeros((M,M,N)) # 3D array of 0s
+x = zeros((M,N),dtype='int64') # 64 bit integers
+
+x = empty((M,N)) # M by N empty array
+x = empty((N,N,N,N)) # 4D empty array
+x = empty((M,N),dtype='float32') # 32-bit floats (single precision)
+
+In = eye(N)
+
