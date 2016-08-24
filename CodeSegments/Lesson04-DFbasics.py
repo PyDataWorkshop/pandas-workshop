c4951d5797f331f197d4f591119a9ff9265703ad
DragonflyStats <kobriendublin@gmail.com>
2016-08-24 23:50:51 +0100

    Update Lesson04-DFbasics.py

--- CodeSegments/Lesson04-DFbasics.py
+++ CodeSegments/Lesson04-DFbasics.py
@@ -106,3 +106,26 @@ myDF.head()
 ##  -- Part19:
 # Select bottom N number of records (default = 5)
 myDF.tail()
+
+
+##########################################
+
+df = pd.DataFrame(
+         {'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]}); 
+df
+  
+df.ix[df.AAA >= 5,'BBB'] = -1; 
+
+df
+
+df.ix[df.AAA >= 5,['BBB','CCC']] = 555; 
+
+df
+
+df.ix[df.AAA < 5,['BBB','CCC']] = 2000; 
+
+df
+
+dflow = df[df.AAA <= 5]
+
+df
