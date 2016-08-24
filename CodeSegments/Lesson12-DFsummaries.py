e1ca68e13d04d2955b2ecf4c38d2aaed63303f5a
DragonflyStats <kobriendublin@gmail.com>
2016-08-24 12:56:24 +0100

    Create Lesson12-DFsummaries.py

---
+++ CodeSegments/Lesson12-DFsummaries.py
@@ -0,0 +1,17 @@
+
+##################################
+
+## Tukey's Five Number Summary
+
+# http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.describe.html
+
+
+
+# The "five number summary", or five statistical summary", consists of 
+# 1. The minimum, 
+# 2. The first quartile
+# 3. The median,
+# 4. The third quartile.
+# 5. The maximum,
+
+myDF.describe()      
