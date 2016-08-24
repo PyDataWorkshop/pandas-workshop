0befd83bf0bcd1f51961a2875334ea3b525cea2a
DragonflyStats <kobriendublin@gmail.com>
2016-08-24 20:42:59 +0100

    Create Lesson09-IFELSE.py

---
+++ Fundamentals/Lesson09-IFELSE.py
@@ -0,0 +1,25 @@
+#########################################
+
+## Conditional IFELSE 
+
+useall = true
+
+
+eltype(useall)
+
+
+#####
+
+X = 7
+Y=X-4
+​
+if useall == false
+    println(X)
+    elseif useall == true
+    println(Y)
+end
+​
+#########################################
+
+
+​
