ccdd181a1b9c6b594b238942d5a31bcaff7e148e
DragonflyStats <kobriendublin@gmail.com>
2016-08-24 20:11:52 +0100

    Create Lesson01-environment.py

---
+++ Fundamentals/Lesson01-environment.py
@@ -0,0 +1,31 @@
+# Checking Your Environment
+
+# preliminaries
+​
+import datetime
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+import pandas as pd
+
+# os- Miscellaneous operating system interfaces
+import os
+
+#####################
+
+print("numpy version {}".format(np.__version__))
+print("matplotlib version {}".format(mpl.__version__))
+print("pandas version {}".format(pd.__version__))
+
+#####################
+
+os.getcwd()
+​
+# os.chdir(path)
+
+
+#####################
+
+​
